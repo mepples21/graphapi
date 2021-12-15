2d019a65878919229fff0ece41b59683ed625b6f
Michael Epping <19227815+mepples21@users.noreply.github.com>
2021-12-15 00:07:08 -0800

    Added content

---
+++ PowerShell/Export-TrustedLocationsToCSV.ps1
@@ -0,0 +1,26 @@
+# Connect-MgGraph -Scopes Policy.Read.All
+
+$namedLocationObjects = @()
+$namedLocationObjects =  Get-MgIdentityConditionalAccessNamedLocation -All | Select-Object *
+
+$output = @()
+
+foreach ($location in $namedLocationObjects)
+{
+
+    $data = @()
+
+    $data = @{
+
+        ranges = $location.AdditionalProperties.ipRanges.cidrAddress -join '; '
+        isTrusted = $location.AdditionalProperties.isTrusted
+        displayName = $location.DisplayName
+        Id = $location.Id
+
+    }
+
+    $output += New-Object psobject -Property $data
+
+}
+
+$output | export-csv -Path .\TrustedLocationOutput.csv -NoTypeInformation -UseQuotes Always
